45d336373e4144ddfa95a7dfb6e74201ca665f23
David van Warmerdam <d.s.v.warmerdam@student.tue.nl>
2024-06-01 14:49:56 +0200

    First itteration optimal optimal_tap_position added functions: 1 - finds min and max tap positions 2 - cycles through them 3 - changes the tap position in the json file

--- src/power_system_simulation/optimal_tap_position.py
+++ src/power_system_simulation/optimal_tap_position.py
@@ -1,16 +1,16 @@
 # Load dependencies and functions from graph_processing
 import copy
 from typing import List, Tuple
+
 import networkx as nx
 
 # Load dependencies and functions from calculation_module
 import pandas as pd
+from calculation_module import *
+from graph_processing import *
 from power_grid_model import CalculationMethod, CalculationType, PowerGridModel, initialize_array, validation
 from power_grid_model.utils import json_deserialize, json_serialize_to_file
 from power_grid_model.validation import ValidationException, assert_valid_batch_data, assert_valid_input_data
-
-from calculation_module import *
-from graph_processing import *
 
 
 def optimal_tap_position(input_network_data: str):
@@ -18,16 +18,16 @@ def optimal_tap_position(input_network_data: str):
     with open(input_network_data) as fp:
         input_data = json_deserialize(fp.read())
 
-    pos_min = np.take(input_data['transformer']['tap_min'],0)
-    pos_max = np.take(input_data['transformer']['tap_max'],0)
+    pos_min = np.take(input_data["transformer"]["tap_min"], 0)
+    pos_max = np.take(input_data["transformer"]["tap_max"], 0)
 
-    for tap_pos in range(pos_max, pos_min+1):
-        input_data['transformer']['tap_pos'] = tap_pos
-        print(input_data['transformer']['tap_pos'])
+    for tap_pos in range(pos_max, pos_min + 1):
+        input_data["transformer"]["tap_pos"] = tap_pos
+        print(input_data["transformer"]["tap_pos"])
 
     return
 
 
 input_network_data = "../data/input_3_test/input_network_data.json"
 
-optimal_tap_position(input_network_data) 
+optimal_tap_position(input_network_data)
